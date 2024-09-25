34f9407f85bbb3c889a1aa2a4b1c30359c1e50a9
herna <sd>
2024-09-25 16:00:49 +0300

    HER-14-dashboard-layout create sidebar and its components

--- app/board/menu.tsx
+++ app/board/menu.tsx
@@ -1,35 +1,42 @@
-import { SidebarItemProps } from "@/types/SidebarItemProps";
-import { GitBranch, Home, Keyboard, NotepadText, Trophy, Users2 } from "lucide-react";
+import { SidebarItemProps } from '@/types/SidebarItemProps'
+import {
+  GitBranch,
+  Home,
+  Keyboard,
+  NotepadText,
+  Trophy,
+  Users2,
+} from 'lucide-react'
 
 export const boardMenu: SidebarItemProps[] = [
-    {
-        label: "Accueil",
-        to: "/board/home",
-        icon: <Home />
-    },
-    {
-        label: "Règlement",
-        to: "/board/outline",
-        icon: <NotepadText />
-    },
-    {
-        label: "Délégations",
-        to: "/board/delegation",
-        icon: <Users2 />
-    },
-    {
-        label: "Tirages",
-        to: "/board/draw",
-        icon: <GitBranch />
-    },
-    {
-        label: "Tableaux",
-        to: "/board/match",
-        icon: <Keyboard />
-    },
-    {
-        label: "Résultats",
-        to: "/board/results",
-        icon: <Trophy />
-    }
+  {
+    label: 'Accueil',
+    to: '/board/home',
+    icon: <Home />,
+  },
+  {
+    label: 'Règlement',
+    to: '/board/outline',
+    icon: <NotepadText />,
+  },
+  {
+    label: 'Délégations',
+    to: '/board/delegation',
+    icon: <Users2 />,
+  },
+  {
+    label: 'Tirages',
+    to: '/board/draw',
+    icon: <GitBranch />,
+  },
+  {
+    label: 'Tableaux',
+    to: '/board/match',
+    icon: <Keyboard />,
+  },
+  {
+    label: 'Résultats',
+    to: '/board/results',
+    icon: <Trophy />,
+  },
 ]
--- components/layouts/sidebar.tsx
+++ components/layouts/sidebar.tsx
@@ -7,13 +7,8 @@ const Sidebar = () => {
   return (
     <aside className='fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex'>
       <nav className='flex flex-col items-center gap-4 px-2 sm:py-5'>
-        {boardMenu.map(({label, to, icon}, _) => (
-          <SidebarItem
-            key={label}
-            label={label}
-            to={to}
-            icon={icon}
-          />
+        {boardMenu.map(({ label, to, icon }, _) => (
+          <SidebarItem key={label} label={label} to={to} icon={icon} />
         ))}
       </nav>
       <nav className='mt-auto flex flex-col items-center gap-4 px-2 sm:py-5'>

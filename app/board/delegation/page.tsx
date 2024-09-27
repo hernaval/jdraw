a1af292b83b698a0dd1fa4ddd4c18c48e9b0b38a
herna <sd>
2024-09-27 23:57:30 +0300

    HER-26 create a filterable athlet list component

--- app/board/delegation/page.tsx
+++ app/board/delegation/page.tsx
@@ -1,14 +1,16 @@
+import AthletCard from '@/components/block/athlet-card'
 import Filter from '@/components/block/filter'
+import getDelegationList from '@/feature/delegation/get-delegation-list'
 import { getClub } from '@/feature/get-club'
 import { getWeighCategory } from '@/feature/get-weigth-category'
 import { mapObSelect } from '@/lib/utils'
 import { FilterSelect } from '@/types/FilterSelect'
+import { AthletEntity } from '@/types/model/athlet'
 import { ClubEntity } from '@/types/model/Club'
 import { WeightCategoryEntity } from '@/types/model/WeightCategory'
-import Link from 'next/link'
 import React, { Suspense } from 'react'
 
-const DelegationList = async () => {
+const DelegationList = async ({ searchParams }: any) => {
   const clubs: ClubEntity[] = await getClub()
   const weights: WeightCategoryEntity[] = await getWeighCategory()
   const filters: FilterSelect[] = [
@@ -24,17 +26,30 @@ const DelegationList = async () => {
     },
   ]
 
+  const athlets: AthletEntity[] = await getDelegationList({
+    club: searchParams.club,
+    category: searchParams.category,
+  })
   return (
-    <div>
+    <div className='mt-6'>
       <Suspense fallback='Loading...'>
         <Filter title='Filtre' filters={filters} />
       </Suspense>
-      <ul>
-        <li>List judoka</li>
-        <li>Tableau categorie/club/nb participants</li>
-        <li>Filter categorie / club</li>
-      </ul>
-      <Link href='/board/delegation/checking'>Charger</Link>
+
+      <section className='mt-8'>
+        <h1 className='text-2xl'>Athlète</h1>
+        <p className='text-primary'>31 athlètes / 15 clubs</p>
+        <div className='flex flex-col md:flex-row mt-10'>
+          <div className='flex-1 grid grid-cols-1 gap-1 md:gap-4 md:grid-cols-2'>
+            {athlets.map((a: AthletEntity) => (
+              <AthletCard athlet={a} key={a.id} />
+            ))}
+          </div>
+          <div className='w-[700px]'>
+            <h2>Summary</h2>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }

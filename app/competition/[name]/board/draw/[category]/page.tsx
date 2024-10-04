d2807e053d0ec82b13b5c4bb2eaacf1b4f40466e
herna <sd>
2024-10-04 11:08:14 +0300

    HER-54 display category with athlet to draw

---
+++ app/competition/[name]/board/draw/[category]/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const DrawCategoryPage = () => {
+  return <div>DrawCategoryPage</div>
+}
+
+export default DrawCategoryPage
--- app/competition/[name]/board/draw/page.tsx
+++ app/competition/[name]/board/draw/page.tsx
@@ -1,21 +1,58 @@
+import AthletCard from '@/components/block/athlet-card'
+import Filter from '@/components/block/filter'
+import { Button } from '@/components/ui/button'
+import getDelegationList from '@/feature/delegation/get-delegation-list'
+import { getWeighCategory } from '@/feature/get-weigth-category'
+import { mapObSelect } from '@/lib/utils'
+import { FilterSelect } from '@/types/FilterSelect'
+import { AthletEntity } from '@/types/model/athlet'
+import { WeightCategoryEntity } from '@/types/model/WeightCategory'
 import Link from 'next/link'
-import React from 'react'
+import React, { Suspense } from 'react'
 
-const DrawList = () => {
+const DrawList = async ({ searchParams, params }: any) => {
+  const competition = params.name
+  const weights: WeightCategoryEntity[] = await getWeighCategory()
+  const filters: FilterSelect[] = [
+    {
+      key: 'category',
+      label: 'Choisir une catégorie',
+      data: mapObSelect(weights, 'id', 'label', 'label', 'sex'),
+      groupable: true,
+    },
+  ]
+
+  const athlets: AthletEntity[] = await getDelegationList({
+    category: searchParams.category,
+    competition: competition,
+  })
+
+  const weightStrToId = (): string => {
+    return (
+      weights.find(w => w.label == searchParams.category)?.id.toString() || ''
+    )
+  }
   return (
     <div>
-      Stepper
-      <ul>
-        <li>Category list</li>
-        <li>next button</li>
-        <li>Dialog / drawer draw format</li>
-        <li>Elimination / round robin custom</li>
-        <li>
-          Custom: Create round and type (Elimination / round robin) with
-          participant number
-        </li>
-        <Link href='/board/draw/athlet'>Start</Link>
-      </ul>
+      <Suspense fallback='Loading...'>
+        <div className='flex justify-between'>
+          <Filter title='Filtre' filters={filters} />
+          <Link
+            href={`/competition/${competition}/board/draw/${weightStrToId()}/format`}>
+            <Button>Configurer le tirage</Button>
+          </Link>
+        </div>
+      </Suspense>
+
+      <div className='mt-8'>
+        <h2 className='text-xl'>Catégorie {searchParams.category}kg</h2>
+        <p>15 athlètes / 5 clubs</p>
+        <div className='flex-1 grid grid-cols-1 gap-1 md:gap-4 md:grid-cols-4 mt-8'>
+          {athlets.map((a: AthletEntity) => (
+            <AthletCard athlet={a} key={a.id} />
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
